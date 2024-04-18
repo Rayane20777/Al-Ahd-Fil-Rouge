6578d71314929944e6b170c15b542e7a5e95c384
Rayane <you@example.com>
2024-04-18 10:09:40 +0100

    base for secretary dashboard added

---
+++ resources/views/secretary/reserved_session.blade.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <title>Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script></head>
+  
+</head>
+<body class="text-gray-800 font-inter">
+    
+    <!-- start: Sidebar -->
+    <div class="fixed left-0 top-0 w-64 h-full bg-gray-900 p-4 z-50 sidebar-menu transition-transform">
+        <a href="#" class="flex items-center pb-4 border-b border-b-gray-800">
+            <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded object-cover">
+            <span class="text-lg font-bold text-white ml-3">Logo</span>
+        </a>
+        <ul class="mt-4">
+            <li class="mb-1 group ">
+                <a href="#" class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
+                    <i class="ri-home-2-line mr-3 text-lg"></i>
+                    <span class="text-sm">Dashboard</span>
+                </a>
+            </li>
+            <li class="mb-1 group">
+                <a href="#" class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
+                    <i class="ri-instance-line mr-3 text-lg"></i>
+                    <span class="text-sm">Users</span>
+                    <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
+                </a>
+                <ul class="pl-7 mt-2 hidden group-[.selected]:block">
+                    <li class="mb-4">
+                        <a href="/admin/doctors" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Doctors</a>
+                    </li> 
+                    <li class="mb-4">
+                        <a href="/admin/users" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Childrens</a>
+                    </li>   
+                </ul>
+            </li>
+            <li class="mb-1 group">
+                <a href="#" class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
+                    <i class="ri-flashlight-line mr-3 text-lg"></i>
+                    <span class="text-sm">Events</span>
+                    <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
+                </a>
+                <ul class="pl-7 mt-2 hidden group-[.selected]:block">
+                    <li class="mb-4">
+                        <a href="/admin/events" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Manage ongoing events</a>
+                    </li>
+                </ul>
+                <ul class="pl-7 mt-2 hidden group-[.selected]:block">
+                    <li class="mb-4">
+                        <a href="/admin/old_events" class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Manage old events</a>
+                    </li>
+                </ul>
+            </li>
+           
+        </ul>
+    </div>
+    <div class="fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden sidebar-overlay"></div>
+    <!-- end: Sidebar -->
+
+    <!-- start: Main -->
+    <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-50 min-h-screen transition-all main">
+        <div class="py-2 px-6 bg-white flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
+            <button type="button" class="text-lg text-gray-600 sidebar-toggle">
+                <i class="ri-menu-line"></i>
+            </button>
+            <ul class="flex items-center text-sm ml-4">
+                <li class="mr-2">
+                    <a href="#" class="text-gray-400 hover:text-gray-600 font-medium">Dashboard</a>
+                </li>
+                <li class="text-gray-600 mr-2 font-medium">/</li>
+                <li class="text-gray-600 mr-2 font-medium">Analytics</li>
+            </ul>
+            <ul class="ml-auto flex items-center">
+                
+                
+                <li class="dropdown ml-3">
+                    <button type="button" class="dropdown-toggle flex items-center">
+                        <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
+                    </button>
+                    <ul class="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]">
+                        <li>
+                            <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Profile</a>
+                        </li>
+                       
+                        <li>
+                            <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="p-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+                <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+                    <div class="flex justify-between mb-6">
+                        <div>
+                            <div class="text-2xl font-semibold mb-1">10</div>
+                            <div class="text-sm font-medium text-gray-400">Active orders</div>
+                        </div>
+                       
+                    </div>
+                   
+                </div>
+                <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+                    <div class="flex justify-between mb-4">
+                        <div>
+                            <div class="flex items-center mb-1">
+                                <div class="text-2xl font-semibold">324</div>
+                                <div class="p-1 rounded bg-emerald-500/10 text-emerald-500 text-[12px] font-semibold leading-none ml-2">+30%</div>
+                            </div>
+                            <div class="text-sm font-medium text-gray-400">Visitors</div>
+                        </div>
+                       
+                    </div>
+                    
+                </div>
+                <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+                    <div class="flex justify-between mb-6">
+                        <div>
+                            <div class="text-2xl font-semibold mb-1"><span class="text-base font-normal text-gray-400 align-top">&dollar;</span>2,345</div>
+                            <div class="text-sm font-medium text-gray-400">Active orders</div>
+                        </div>
+                        
+                    </div>
+                    <a href="#" class="text-blue-500 font-medium text-sm hover:text-blue-600">View details</a>
+                </div>
+            </div>
+            <div >
+                <div class="bg-white border border-gray-100 shadow-md shadow-black/5 p-6 rounded-md">
+                    <div class="flex justify-between mb-4 items-start">
+                        <div class="font-medium">Manage orders</div>
+                       
+                    </div>
+                   
+                    <div class="overflow-x-auto">
+                        <table class="w-full min-w-[540px]" data-tab-for="order" data-page="active">
+                            <thead>
+                                <tr>
+                                    <th class="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left rounded-tl-md rounded-bl-md">Service</th>
+                                    <th class="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left">Estimate</th>
+                                    <th class="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left">Budget</th>
+                                    <th class="text-[12px] uppercase tracking-wide font-medium text-gray-400 py-2 px-4 bg-gray-50 text-left rounded-tr-md rounded-br-md">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="py-2 px-4 border-b border-b-gray-50">
+                                        <div class="flex items-center">
+                                            <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded object-cover block">
+                                            <a href="#" class="text-gray-600 text-sm font-medium hover:text-blue-500 ml-2 truncate">Create landing page</a>
+                                        </div>
+                                    </td>
+                                    <td class="py-2 px-4 border-b border-b-gray-50">
+                                        <span class="text-[13px] font-medium text-gray-400">3 days</span>
+                                    </td>
+                                    <td class="py-2 px-4 border-b border-b-gray-50">
+                                        <span class="text-[13px] font-medium text-gray-400">$56</span>
+                                    </td>
+                                    <td class="py-2 px-4 border-b border-b-gray-50">
+                                        <span class="inline-block p-1 rounded bg-emerald-500/10 text-emerald-500 font-medium text-[12px] leading-none">In progress</span>
+                                    </td>
+                                </tr>
+                                
+                        </table>
+                       
+                    </div>
+                </div>
+                
+            </div>
+
+
+
+
+
+        </div>
+    </main>
+    <!-- end: Main -->
+
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="dist/js/script.js"></script>
+
+    <script>
+     // start: Sidebar
+const sidebarToggle = document.querySelector('.sidebar-toggle')
+const sidebarOverlay = document.querySelector('.sidebar-overlay')
+const sidebarMenu = document.querySelector('.sidebar-menu')
+const main = document.querySelector('.main')
+if(window.innerWidth < 768) {
+    main.classList.toggle('active')
+    sidebarOverlay.classList.toggle('hidden')
+    sidebarMenu.classList.toggle('-translate-x-full')
+}
+sidebarToggle.addEventListener('click', function (e) {
+    e.preventDefault()
+    main.classList.toggle('active')
+    sidebarOverlay.classList.toggle('hidden')
+    sidebarMenu.classList.toggle('-translate-x-full')
+})
+sidebarOverlay.addEventListener('click', function (e) {
+    e.preventDefault()
+    main.classList.add('active')
+    sidebarOverlay.classList.add('hidden')
+    sidebarMenu.classList.add('-translate-x-full')
+})
+document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault()
+        const parent = item.closest('.group')
+        if (parent.classList.contains('selected')) {
+            parent.classList.remove('selected')
+        } else {
+            document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (i) {
+                i.closest('.group').classList.remove('selected')
+            })
+            parent.classList.add('selected')
+        }
+    })
+})
+// end: Sidebar
+
+
+
+// start: Popper
+const popperInstance = {}
+document.querySelectorAll('.dropdown').forEach(function (item, index) {
+    const popperId = 'popper-' + index
+    const toggle = item.querySelector('.dropdown-toggle')
+    const menu = item.querySelector('.dropdown-menu')
+    menu.dataset.popperId = popperId
+    popperInstance[popperId] = Popper.createPopper(toggle, menu, {
+        modifiers: [
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, 8],
+                },
+            },
+            {
+                name: 'preventOverflow',
+                options: {
+                    padding: 24,
+                },
+            },
+        ],
+        placement: 'bottom-end'
+    });
+})
+document.addEventListener('click', function (e) {
+    const toggle = e.target.closest('.dropdown-toggle')
+    const menu = e.target.closest('.dropdown-menu')
+    if (toggle) {
+        const menuEl = toggle.closest('.dropdown').querySelector('.dropdown-menu')
+        const popperId = menuEl.dataset.popperId
+        if (menuEl.classList.contains('hidden')) {
+            hideDropdown()
+            menuEl.classList.remove('hidden')
+            showPopper(popperId)
+        } else {
+            menuEl.classList.add('hidden')
+            hidePopper(popperId)
+        }
+    } else if (!menu) {
+        hideDropdown()
+    }
+})
+
+function hideDropdown() {
+    document.querySelectorAll('.dropdown-menu').forEach(function (item) {
+        item.classList.add('hidden')
+    })
+}
+function showPopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+        return {
+            ...options,
+            modifiers: [
+                ...options.modifiers,
+                { name: 'eventListeners', enabled: true },
+            ],
+        }
+    });
+    popperInstance[popperId].update();
+}
+function hidePopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+        return {
+            ...options,
+            modifiers: [
+                ...options.modifiers,
+                { name: 'eventListeners', enabled: false },
+            ],
+        }
+    });
+}
+// end: Popper
+
+
+
+
+    </script>
+</body>
+</html>
